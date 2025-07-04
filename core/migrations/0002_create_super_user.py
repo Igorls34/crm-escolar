c44c42865ecbbec5fc20f3bac4adcf947cb237f1
Igorls34 <igorlaurindo49@gmail.com>
2025-07-04 17:42:39 -0300

    Adiciona data migration para criar superuser automaticamente

--- core/migrations/0002_create_super_user.py
+++ core/migrations/0002_create_super_user.py
@@ -1,20 +1,19 @@
+from django.contrib.auth import get_user_model
 from django.db import migrations
 
 def create_super_user(apps, schema_editor):
-    # Importa o modelo real de User
-    User = apps.get_model('auth', 'User')
+    User = get_user_model()
     if not User.objects.filter(username='admin').exists():
         User.objects.create_superuser(
             username='Igor',
             email='igorlaurindo49@gmail.com',
-            password='INTERATECH'
+            password='admin@2025b'
         )
 
 class Migration(migrations.Migration):
     dependencies = [
         ('core', '0001_initial'),
     ]
-
     operations = [
         migrations.RunPython(create_super_user),
     ]
